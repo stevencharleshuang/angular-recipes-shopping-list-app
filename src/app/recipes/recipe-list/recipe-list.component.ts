c4544ed8310cf412aa69b2cce445c4929acc963c
Steven Huang <steven.charles.huang@gmail.com>
2018-10-16 23:36:49 -0400

    added output of recipes array in recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    // calls new on Recipe class constructor
+    new Recipe('A Test Recipe', 'This is just a test', 'http://downloads.bbc.co.uk/skillswise/entry12/english/en05skim/images/en05skim-e2-w-scanning-a-recipe-tikka-masala-592x838.jpg'),
+    new Recipe('Another Recipe', 'Stupid test recipe', 'https://www.paprikaapp.com/media/web/images/help/windows/recipe.png'),
+  ];
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,7 +5,7 @@ export class Recipe {
   public description: string;
   public imagePath: string;
 
-  constructor(name: string, description string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
