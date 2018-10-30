ebcd08048f098a32864f93f7433dd6568e7696df
Steven Huang <steven.charles.huang@gmail.com>
2018-10-30 00:05:37 -0400

    added click events for navigation in header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-recipes-shopping-list-app';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
